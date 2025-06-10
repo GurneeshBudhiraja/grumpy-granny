9ed35e8a6f010290feeb578d9836bd260029f594
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2025-06-10 15:48:48 -0700

    Clear Screen and Add Wall Background

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,34 +1,17 @@
-import { useEffect, useState } from 'react';
 import { useClickSound } from './hooks/useClickSound';
 import { useKeyboardSound } from './hooks/useKeyboardSound';
 
 export const App = () => {
-  const [breathing, setBreathing] = useState(false);
-  
   // Add global click sound effect
   useClickSound();
   
   // Add global keyboard sound effect
   useKeyboardSound();
 
-  useEffect(() => {
-    const breathingTimer = setTimeout(() => {
-      setBreathing(!breathing);
-    }, 500);
-
-    return () => {
-      clearTimeout(breathingTimer);
-    };
-  }, [breathing]);
-
   return (
-    <div className=''>
-      Hello
-      {breathing ? (
-        <img src="./granny-idle.png" className="w-64 h-64 object-contain contrast-115 brightness-90 sepia-10 grayscale-15 drop-shadow" />
-      ) : (
-        <img src="./granny-blink.png" className="w-64 h-64 object-contain contrast-115 brightness-90 sepia-10 grayscale-15 drop-shadow" />
-      )}
+    <div className="min-h-screen w-full bg-cover bg-center bg-no-repeat relative overflow-hidden" 
+         style={{ backgroundImage: 'url(/wall-background.png)' }}>
+      {/* Scene content will go here */}
     </div>
   );
 };
