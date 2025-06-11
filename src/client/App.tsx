00b81374f855370a5512205e830b3ef9d2ae12da
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2025-06-10 19:16:29 -0700

    Updated App.tsx

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -24,8 +24,6 @@ export const App = () => {
         }}
       >
         {/* Monitor Screen Overlay - Positioned inside the monitor frame with reduced opacity */}
-
-        
         <div
           className="absolute inset-0 bg-contain bg-no-repeat bg-center opacity-40"
           style={{
