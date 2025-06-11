c90fd7ceb8ee307579c0f928259045936c1d76f0
GurneeshBudhiraja <gurneeshbudhiraja@gmail.com>
2025-06-10 19:08:41 -0700

    add the monito screen

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -17,20 +17,12 @@ export const App = () => {
       />
 
       {/* Computer Monitor - Bottom Center, Always Responsive */}
-      <div className="absolute -bottom-[180px] left-1/2 transform -translate-x-1/2 w-full max-w-6xl px-2 sm:px-4 pb-2 sm:pb-4">
-        <div className="relative w-full flex justify-center">
-          {/* Computer Monitor Image - Responsive */}
-          <img
-            src="/computer-monitor.png"
-            alt="Vintage Computer Monitor"
-            className="w-full max-w-24xl sm:max-w-3xl md:max-w-4xl lg:max-w-5xl h-auto object-contain drop-shadow-2xl"
-            style={{ maxHeight: '100vh' }}
-          />
-        </div>
-      </div>
-
-      {/* Atmospheric Room Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-b from-black/20 via-transparent to-black/40 pointer-events-none"></div>
+      <div
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-2/3 max-w-lg h-auto bg-contain bg-no-repeat bg-center z-20"
+        style={{
+          backgroundImage: "url('/computer-monitor.png')",
+        }}
+      ></div>
     </div>
   );
 };
