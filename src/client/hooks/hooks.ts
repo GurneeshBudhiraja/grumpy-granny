00317565929b47c6db04a5b1f1f0e4c72defe958
Gurneesh Budhiraja <gurneeshbudhiraja@gmail.com>
2025-06-30 07:09:21 -0700

    Updated hooks.ts

--- src/client/hooks/hooks.ts
+++ src/client/hooks/hooks.ts
@@ -1,6 +1,5 @@
 import { useClickSound } from './useClickSound';
 import { useKeyboardSound } from './useKeyboardSound';
 import { useGrannyAnimation } from './useGrannyAnimation';
-import { useFakeCursor } from './useFakeCursor';
 import { useThemeSong } from './useThemeSong';
-export { useClickSound, useKeyboardSound, useGrannyAnimation, useFakeCursor, useThemeSong };
+export { useClickSound, useKeyboardSound, useGrannyAnimation, useThemeSong };
